ef2c325c896c547cf47e79cfc3bc586997dffea7
Hector Martinez <hectormartinezresendiz@gmail.com>
2020-12-19 10:46:31 -0600

    Connection with RegistrationVacancies

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Input.scss'
+import { AiOutlineUser } from "react-icons/ai"
 import { MdSearch, MdFlag } from "react-icons/md";
 import { FaMoneyBillWaveAlt } from "react-icons/fa";
+import { RiLockPasswordLine } from "react-icons/ri"
 
 const Input = ({ inputType, inputRef, inputName, inputValue, inputPlaceHolder, inputOnChange, inputBorder = ''} ) => (
   <div className="input__content">
     { inputName==="market-stall" && <MdSearch className="input__icon"/> }
     { inputName==="location" && <MdFlag /> }
     { inputName==="salary-range" && <FaMoneyBillWaveAlt /> }
+    { inputName==="user" && <AiOutlineUser />}
+    { inputName==="password" && <RiLockPasswordLine />}
     <input type={inputType} ref={inputRef} name={inputName} value={inputValue} placeholder={inputPlaceHolder} onChange={inputOnChange} className="input" style={{ border: inputBorder }}/>
   </div>
 )
--- src/pages/RegistrationVacancies/RegistrationVacancies.jsx
+++ src/pages/RegistrationVacancies/RegistrationVacancies.jsx
@@ -4,8 +4,33 @@ import Card from '../../components/Card/Card'
 import Input from '../../components/Input/Input'
 import Image from '../../components/Image/Image'
 import Button from '../../components/Button/Button'
+// import Axios from 'axios'
 
+// const RegistrationVacancies = async () => {
 const RegistrationVacancies = () => {
+
+  // const url = "https://hpm-devars-falconmasters-backend.vercel.app/api/vacancies";
+  // const body = {
+  //   "idCompany": "NOMBRE DE LA COMPAÑIA",
+  //   "branch": "BACKEND",
+  //   "country": "México",
+  //   "urlImage":
+  //     "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Platzi.jpg/1920px-Platzi.jpg",
+  //   "position": "DESAROLLADOR FRONTEND",
+  //   "salary": 100000,
+  //   "coin": "MXN",
+  //   "requirements": "Estudiar en Platzi Master",
+  //   "skills": "Alto conocimiento y manejo de la suite de Adobe (Ilustrator, Photoshop, indesign, audiovisual). Desarrollo de diseño publicitario, identidad corporativa, packaging y para aplicaciones digitales (diseño web, redes sociales, apps). Conocimiento y adaptación en tendencias de diseño.Excelente ortografía.Administración del tiempo. Conocimientos básicos en Mercadotecnia",
+  //   "details": "Forma parte un gran equipo de trabajo dentro de una Agencia de mercadotecnia y publicidad. Eres Diseñador gráfico con experiencia en diseño publicitario e identidad corporativa, con alto grado de conocimiento en edición fotográfica en Adobe Photoshop e ilustración en Adobe Illustrator, te estamos buscando",
+  //   "tags": ["Fronted", "Ilustrator", "Photoshop", "Mercadotecnia", "diseño"],
+  //   "emailBoss": "boss@company.com"
+  // }
+  // // headers = {
+  // //   headers: { Authorization: `Bearer ${token}` },
+  // // };
+  // const result = await Axios.post(url, body);
+  // console.log(result.status);
+
   return (
     <div className="registration__vacancies">
       <Card cardWidth="900px" cardHeight="auto">
