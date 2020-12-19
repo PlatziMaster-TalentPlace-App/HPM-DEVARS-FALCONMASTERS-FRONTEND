114a6365a923a98e39069ae8c278bddab89ea0a9
Hector Martinez <hectormartinezresendiz@gmail.com>
2020-12-18 21:50:31 -0600

    Creating of page RegistrationVacancies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import User from '../pages/User/User'
+import RegistrationVacancies from '../pages/RegistrationVacancies/RegistrationVacancies'
 import Error404 from '../pages/Error404/Error404'
 import Layout from '../components/Layout/Layout'
 
@@ -11,6 +12,7 @@ const App = () => (
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/user" component={User}/>
+                <Route exact path="/registration-vacancies" component={RegistrationVacancies}/>
                 <Route component={Error404} />
             </Switch>
         </Layout>
