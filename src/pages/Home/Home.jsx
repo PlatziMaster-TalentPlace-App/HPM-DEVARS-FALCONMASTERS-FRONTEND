b551c6dd0eff6821c23ba61e69544c4a9b806cdd
Hector Martinez <hectormartinezresendiz@gmail.com>
2020-12-19 02:48:10 -0600

    API consumer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,55 +5,6 @@ import Table from '../../components/Table/Table'
 import Banner from '../../components/Banner/Banner'
 import AppContext from '../../context/AppContext'
 
-const options = {
-  vacancies: [
-    {
-      id: 1,
-      career: "FULLSTACK",
-      name: "NASA",
-      urlImage:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1200px-NASA_logo.svg.png",
-      vacant: "Desarrollador fullstack",
-      salary: "30,000",
-      coin: "MXN",
-      status: "En proceso",
-    },
-    {
-      id: 2,
-      career: "FRONTEND",
-      name: "IBM DE MÉXICO",
-      urlImage:
-        "https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg",
-      vacant: "Frontend developer",
-      salary: "8,000,000",
-      coin: "COP",
-      status: "Vacante cerrada",
-    },
-    {
-      id: 3,
-      career: "BACKEND",
-      name: "NINTENDO",
-      urlImage:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Nintendo_red_logo.svg/406px-Nintendo_red_logo.svg.png",
-      vacant: "Backend",
-      salary: "3,000",
-      coin: "DLL",
-      status: "¡Te seleccionarón!",
-    },
-    {
-      id: 4,
-      career: "DATA SCIENCE",
-      name: "PLATZI",
-      urlImage:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Platzi.jpg/800px-Platzi.jpg",
-      vacant: "Data Science",
-      salary: "100,000",
-      coin: "MXN",
-      status: "En proceso",
-    },
-  ],
-};
-
 const Home = () => {
   const { vacancies } = useContext(AppContext)
   return (
@@ -63,7 +14,7 @@ const Home = () => {
       <div className="home__tables">
         <Table
           title="VACANTES DISPONIBLES"
-          options={options.vacancies}
+          options={vacancies.data}
           buttonTitle="POSTULARME"
           listGridTemplateColumns="1fr 1fr 1fr 1fr"
         />
@@ -71,7 +22,7 @@ const Home = () => {
           application="true"
           title="MIS ULTIMAS APLICACIONES"
           cardWidth="400px"
-          options={options.vacancies}
+          options={vacancies.data}
           listGridTemplateColumns="1fr 1fr 1fr"
         />
       </div>
