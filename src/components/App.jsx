e1d126878524534632de108152249c926e6ee3e4
Ricardo Tornero <richardo@outlook.com>
2020-12-18 21:37:51 -0600

    First version Vacant Page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Error404 from '../pages/Error404/Error404'
 import Layout from '../components/Layout/Layout'
+import Vacant from '../pages/Vacant/Vacant';
 
 const App = () => (
     <BrowserRouter>
         <Layout>
             <Switch>
                 <Route exact path="/" component={Home}/>
+                <Route exact path="/vacant" component={Vacant} />
                 <Route component={Error404} />
             </Switch>
         </Layout>
